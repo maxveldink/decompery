7baade606bfdfa61237bf50fdb987ca14dd9c9f3
Max VelDink <max@audaciousaardvark.com>
2022-10-21 06:03:56 -0400

    Add decomp table

---
+++ db/migrate/20221021094028_enable_uuid.rb
@@ -0,0 +1,8 @@
+# typed: false
+# frozen_string_literal: true
+
+class EnableUuid < ActiveRecord::Migration[7.0]
+  def change
+    enable_extension "pgcrypto"
+  end
+end
---
+++ db/migrate/20221021094029_create_decomps.rb
@@ -0,0 +1,12 @@
+# typed: true
+# frozen_string_literal: true
+
+class CreateDecomps < ActiveRecord::Migration[7.0]
+  def change
+    create_table :decomps, id: :uuid do |t|
+      t.text :topic
+
+      t.timestamps
+    end
+  end
+end
