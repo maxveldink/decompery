dce8ade7407840a68e92d6714966724639e18426
Max VelDink <max@audaciousaardvark.com>
2022-11-01 20:04:30 -0400

    Update deps

--- test/system/decomps_test.rb
+++ test/system/decomps_test.rb
@@ -6,37 +6,46 @@ require "application_system_test_case"
 class DecompsTest < ApplicationSystemTestCase
   test "decomp can be created" do
     visit root_url
+
     assert_text "Decomps"
 
     click_on "New decomp"
+
     assert_text "Topic"
 
     fill_in "Topic", with: "Testing 123"
     click_on "Create Decomp"
+
     assert_text "Testing 123"
 
     click_on "Decay something else"
+
     assert_text "Decomps"
   end
 
   test "link to existing decomp appears once visited" do
     visit root_url
+
     assert_no_text "currently decomposing"
 
     click_on "New decomp"
     fill_in "Topic", with: "Testing 123"
     click_on "Create Decomp"
+
     assert_text "Testing 123"
 
     visit root_url
+
     assert_text "currently decomposing"
 
     click_on "currently decomposing"
+
     assert_text "Testing 123"
   end
 
   test "adding additional stages of decomp" do
     visit decomp_path(decomps(:roasting_max))
+
     assert_text decomps(:roasting_max).topic
     assert_no_text "A sub bullet"
 
