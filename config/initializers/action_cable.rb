1d2f2cd9e664bfb73869a4018975b946805e0ffc
Max VelDink <max@audaciousaardvark.com>
2022-11-01 20:00:54 -0400

    Add redis reset fix

---
+++ config/initializers/action_cable.rb
@@ -0,0 +1,23 @@
+# typed: false
+# frozen_string_literal: true
+
+# Restart Action Cable server on Redis connection failures.
+# See: https://github.com/rails/rails/pull/45478
+require "action_cable/subscription_adapter/redis"
+
+module ActionCableRedisListenerPatch
+  private
+
+  def ensure_listener_running
+    @thread ||= Thread.new do # rubocop:disable Naming/MemoizedInstanceVariableName
+      Thread.current.abort_on_exception = true
+      conn = @adapter.redis_connection_for_subscriptions
+      listen conn
+    rescue ::Redis::BaseConnectionError
+      @thread = @raw_client = nil
+      ::ActionCable.server.restart
+    end
+  end
+end
+
+ActionCable::SubscriptionAdapter::Redis::Listener.prepend(ActionCableRedisListenerPatch)
---
+++ sorbet/rbi/shims/action_cable.rbi
@@ -0,0 +1,9 @@
+module ActionCable
+  module SubscriptionAdapter
+    module Redis
+      class Listener
+        def self.prepend(*); end
+      end
+    end
+  end
+end
