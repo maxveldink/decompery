484890657cc48db52b454c4d29fdbc70849791dc
Max VelDink <max@audaciousaardvark.com>
2022-11-01 21:45:19 -0400

    Add invite link

---
+++ app/javascript/controllers/clipboard_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = {
+    contentsToCopy: String
+  }
+
+  async copy() {
+    await navigator.clipboard.writeText(this.contentsToCopyValue)
+
+    const oldText = this.element.textContent;
+    this.element.textContent = "🍄 Copied!"
+    setTimeout(() => { this.element.textContent = oldText }, 2000)
+  }
+}
